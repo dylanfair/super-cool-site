f1fe7a6d4d9acca62d5a38b9ddc878c0c3a9e7c2
Dylan <carringtonfair.dylan@gmail.com>
2024-07-26 22:47:05 -0400

    got some basic transition slides going on now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,48 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	let activate_slide_out = false;
+	let activate_slide_in = false;
+	let first_hide = false;
+	let second_hide = true;
 
-<div class="h-screen flex space-x-5 justify-center items-center">
-	<div class="space-y-5 text-center">
-		<h1 class="h1">oh, hey</h1>
+	function reset() {
+		activate_slide_out = false;
+		activate_slide_in = false;
+		first_hide = false;
+		second_hide = true;
+	}
+
+	function delayed_hide() {
+		first_hide = true;
+	}
+
+	function first_transition() {
+		activate_slide_out = true;
+
+		setTimeout(delayed_hide, 1000);
+		second_hide = false;
+		activate_slide_in = true;
+
+	}
+</script>
+
+<div class="h-60 flex space-x-5 justify-center items-center">
+	<div id="section1" class="absolute flex space-x-5 justify-center items-center" class:animate-slide-out={activate_slide_out} class:invisible={first_hide}>
+		<div class="space-y-5 text-center">
+			<h1 class="h1">oh, hey</h1>
+		</div>
+		<div class="space-y-5 text-center">
+			<div class="animate-bounce variant-filled-secondary rounded-full">
+				<button on:click={first_transition}>Click me!</button>
+			</div>
+			<p>You should really click it</p>
+		</div>
 	</div>
-	<div class="space-y-5 text-center">
-    <div class="animate-bounce variant-filled-secondary rounded-full">
-      <button>Click me!</button>
-    </div>
-		<p>You should really click it</p>
+	<div id="section2" class="absolute" class:animate-slide-in={activate_slide_in} class:invisible={second_hide}>
+		<div class="space-y-5 text-center">
+			<h1 class="h1">I'm a different section</h1>
+		</div>
 	</div>
+</div>
+<div class="flex justify-center items-center">
+	<button on:click={reset}>Reset</button>
 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,35 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
 		extend: {
+      keyframes: {
+        slide_out: {
+          '0%' : {
+            transform: 'translateX(0px)',
+            opacity: '1',
+          },
+          '100%' : {
+            transform: 'translateX(-300px)',
+            opacity: '0',
+          },
+        },
+
+        slide_in: {
+          '0%' : {
+            transform: 'translateX(300px)',
+            opacity: '0',
+          },
+          '100%' : {
+            transform: 'translateX(0px)',
+            opacity: '1',
+          },
+        },
+        
+      },
       animation: {
-        'bounce-short': 'bounce 1s ease-in-out 1'
+        'bounce-short': 'bounce 1s ease-in-out 1',
+        'spin-slow': 'spin 3s linear infinite',
+        'slide-out': 'slide_out 1s ease',
+        'slide-in': 'slide_in 2s ease'
       }
     },
 	},
