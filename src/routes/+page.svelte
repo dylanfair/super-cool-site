79eb85f2863a0d6c73a32fdc5b6bbd78e86e66d2
Dylan <carringtonfair.dylan@gmail.com>
2024-10-06 18:15:01 -0400

    trying to make the code more re-usable in case I need a lot of divs/transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,60 @@
-<script>
-	let activate_slide_out = false;
-	let activate_slide_in = false;
-	let first_hide = false;
-	let second_hide = true;
+<script lang="ts">
+
+  function Section(id, hide, slide_out, slide_in) {
+    this.id = id;
+    this.hide = hide;
+    this.slide_out = slide_out;
+    this.slide_in = slide_in;
+    this.toggleHide = function() {
+      this.hide = true; 
+    }
+  };
+
+  var section1 = new Section(1, false, false, false);
+  var section2 = new Section(2, true, false, false);
 
 	function reset() {
-		activate_slide_out = false;
-		activate_slide_in = false;
-		first_hide = false;
-		second_hide = true;
+    section1 = new Section(1, false, false, false);
+    section2 = new Section(2, true, false, false);
 	}
 
-	function delayed_hide() {
-		first_hide = true;
-	}
+	function first_transition(sec1, sec2) {
+    console.log("Doing a transition");
+    console.log(sec1);
+    console.log(sec2);
+		sec1.slide_out = true;
 
-	function first_transition() {
-		activate_slide_out = true;
-
-		setTimeout(delayed_hide, 1000);
-		second_hide = false;
-		activate_slide_in = true;
-
+    if (!sec1.hide) {
+      setTimeout(() => {sec1.hide = true; }, 1000);
+    }
+		sec2.hide = false;
+		sec2.slide_in = true;
+    console.log(sec1);
+    console.log(sec2);
 	}
 </script>
 
 <div class="h-screen flex space-x-5 justify-center items-center">
-	<div id="section1" class="absolute flex space-x-5 justify-center items-center" class:animate-slide-out={activate_slide_out} class:invisible={first_hide}>
+
+	<div id="section1" class="absolute flex space-x-5 justify-center items-center" class:animate-slide-out={section1.slide_out} class:invisible={section1.hide}>
 		<div class="space-y-5 text-center">
 			<h1 class="h1">oh, hey</h1>
 		</div>
 		<div class="space-y-5 text-center">
 			<div class="animate-bounce variant-filled-secondary rounded-full">
-				<button on:click={first_transition}>Click me!</button>
+				<button on:click={() => first_transition(section1, section2)}>Click me!</button>
 			</div>
-			<p>You should really click it</p>
+			<p>(You should really click it)</p>
 		</div>
 	</div>
-	<div id="section2" class="absolute" class:animate-slide-in={activate_slide_in} class:invisible={second_hide}>
+
+	<div id="section2" class="absolute" class:animate-slide-in={section2.slide_in} class:invisible={section2.hide}>
 		<div class="space-y-5 text-center">
 			<h1 class="h1">I'm a different section</h1>
 		</div>
 	</div>
 </div>
+
 <div class="absolute left-0 top-0">
 	<button on:click={reset}>Reset</button>
 </div>
