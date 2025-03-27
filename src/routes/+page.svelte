f766cb3c4e05014ea4eb5a2e3b178b876b086129
Dylan <carringtonfair.dylan@gmail.com>
2025-03-26 21:14:49 -0400

    using proper constructor now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,70 +1,74 @@
 <script lang="ts">
 
-  function Section(id, hide, slide_out, slide_in) {
-    this.id = id;
-    this.hide = hide;
-    this.slide_out = slide_out;
-    this.slide_in = slide_in;
-    this.toggleHide = function() {
-      this.hide = true; 
-    }
-  };
+	class Section {
+		id: number;
+		hide: boolean;
+		slide_out: boolean;
+		slide_in: boolean;
 
-  var section1 = new Section(1, false, false, false);
-  var section2 = new Section(2, true, false, false);
-  var section3 = new Section(3, true, false, false);
-  var section4 = new Section(4, true, false, false);
-  var section5 = new Section(5, true, false, false);
+		constructor(id: number, hide: boolean, slide_out: boolean, slide_in: boolean) {
+			this.id = id;
+			this.hide = hide;
+			this.slide_out = slide_out;
+			this.slide_in = slide_in;
+		}
+	}
+
+	let section1 = new Section(1, false, false, false);
+	let section2 = new Section(2, true, false, false);
+	let section3 = new Section(3, true, false, false);
+	let section4 = new Section(4, true, false, false);
+	let section5 = new Section(5, true, false, false);
 
 	function reset() {
-    section1 = new Section(1, false, false, false);
-    section2 = new Section(2, true, false, false);
-    section3 = new Section(3, true, false, false);
-    section4 = new Section(4, true, false, false);
-    section5 = new Section(5, true, false, false);
+		section1 = new Section(1, false, false, false);
+		section2 = new Section(2, true, false, false);
+		section3 = new Section(3, true, false, false);
+		section4 = new Section(4, true, false, false);
+		section5 = new Section(5, true, false, false);
 	}
 
   // just hard coding this because idk how any of this works
 	function first_transition() {
-    console.log("First transition");
+		console.log("First transition");
 		section1.slide_out = true;
 
-    if (!section1.hide) {
-      setTimeout(() => {section1.hide = true; }, 1000);
-    }
+		if (!section1.hide) {
+			setTimeout(() => {section1.hide = true; }, 1000);
+		}
 		section2.hide = false;
 		section2.slide_in = true;
 	}
 
 	function second_transition() {
-    console.log("Second transition");
+		console.log("Second transition");
 		section2.slide_out = true;
 
-    if (!section2.hide) {
-      setTimeout(() => {section2.hide = true; }, 1000);
-    }
+		if (!section2.hide) {
+			setTimeout(() => {section2.hide = true; }, 1000);
+		}
 		section3.hide = false;
 		section3.slide_in = true;
 	}
 
 	function third_transition() {
-    console.log("Third transition");
+		console.log("Third transition");
 		section3.slide_out = true;
 
-    if (!section3.hide) {
-      setTimeout(() => {section3.hide = true; }, 1000);
-    }
+		if (!section3.hide) {
+			setTimeout(() => {section3.hide = true; }, 1000);
+		}
 		section4.hide = false;
 		section4.slide_in = true;
 	}
 
 	function fourth_transition() {
-    console.log("Fourth transition");
+		console.log("Fourth transition");
 		section4.slide_out = true;
 
-    if (!section4.hide) {
-      setTimeout(() => {section4.hide = true; }, 1000);
-    }
+		if (!section4.hide) {
+			setTimeout(() => {section4.hide = true; }, 1000);
+		}
 		section5.hide = false;
 		section5.slide_in = true;
 	}
